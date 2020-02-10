83b00a603889e056b305582ddbbbdec445756bf5
Maikel Scheffer <mjbt.scheffer@gmail.com>
2020-02-10 13:20:16 +0100

    Enforce personal linting preferences

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,20 @@ module.exports = {
   extends: [
     'standard',
     'plugin:vue/recommended' // or 'plugin:vue/base'
-  ]
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 3,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off'
+  }
 };
