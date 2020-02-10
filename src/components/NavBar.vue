8587ca4ef44ff3d59689f776f66d4dc293f227c0
Maikel Scheffer <mjbt.scheffer@gmail.com>
2020-02-10 14:12:08 +0100

    Detached styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,16 @@
 <template>
   <nav class="navbar custom__navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="https://www.thuisbezorgd.nl/">
-        <img src="../assets/logo.svg" width="112" height="28" />
-      </a>
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://www.thuisbezorgd.nl/">
+          <img src="../assets/logo.svg" width="112" height="28" />
+        </a>
+      </div>
     </div>
   </nav>
 </template>
 
 <script>
-import '../style/_navbar.scss';
-
 export default {
   name: 'NavBar'
 };
