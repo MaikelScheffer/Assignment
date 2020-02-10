2813322451c2db6aa256de76a176c5cafeb95ae0
Maikel Scheffer <mjbt.scheffer@gmail.com>
2020-02-10 13:24:06 +0100

    Add internationalization options

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+import router from './router';
+import store from './store';
+import i18n from './i18n';
+
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 import '@fortawesome/fontawesome-free/js/all.js';
-
 import './style/main.scss';
-import router from './router';
-import store from './store';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app');
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   css: {
     sourceMap: true
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'nl-NL',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
   }
 }
